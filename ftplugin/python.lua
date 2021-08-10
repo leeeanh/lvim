7159ad716d7495d9f2d6e8154eb2df3357151611
leeanh <leeanh3011@gmail.com>
2021-08-10 20:49:03 +0700

    Remove roots setting for python file

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -3,6 +3,10 @@ lvim.lang.python.formatters = {
     exe = "black",
     args = {},
   },
+  {
+    exe = "isort",
+    args = {},
+  },
 }
 
 lvim.lang.python.linters = {
@@ -11,18 +15,3 @@ lvim.lang.python.linters = {
     args = {},
   },
 }
-
-local root_files = {
-    'setup.py',
-    'pyproject.toml',
-    'setup.cfg',
-    'requirements.txt',
-    '.git',
-    'BUILD.bazel',
-}
-
-local util = require 'lspconfig/util'
-lvim.lang.python.lsp.setup.root_dir = function(filename)
-      return util.root_pattern(unpack(root_files))(filename) or
-              util.path.dirname(filename)
-            end
